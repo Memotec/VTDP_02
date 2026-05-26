bfa517e2704c2e34586cc780bd53297e7374de53
Memotec <41383155+Memotec@users.noreply.github.com>
2026-05-26 13:37:19 +0700

    chore: remove base path from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-export default defineConfig({
-  base: '/VTDP_02/'
-})
+
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
