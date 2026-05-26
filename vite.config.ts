334accd82d3fb9ceea934b5c0dfecdea038abd4c
Memotec <41383155+Memotec@users.noreply.github.com>
2026-05-26 13:42:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/VTDP_02/'
+})
 
 export default defineConfig(() => {
   return {
